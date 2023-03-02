43397ddd357b8ebef91510d56fe72d173830f17f
Stephen <stephenzhang003@gmail.com>
2023-03-01 19:15:50 -0500

    Added the ability to pass the state of content in a post except category id and resume

--- client/src/pages/PostCreate.jsx
+++ client/src/pages/PostCreate.jsx
@@ -1,63 +1,123 @@
 import Header from '../components/Header';
-import { Container, Menu, TextInput, Textarea, FileInput, Divider, Button } from '@mantine/core';
+import { useState } from "react";
+import { Container, Menu, TextInput, Textarea, FileInput, Divider, Button, Checkbox, Select } from '@mantine/core';
+import { useNavigate } from 'react-router-dom';
+import * as postapi from "../api/index"
 
 function PostCreate() {
+    // const [title, setTitle] = useState("");
+    // const [outcome, setOutcome] = useState("");
+    // const [content, setContent] = useState("");
+    // const [author, setAuthor] = useState("");
+    // const [category_id, setCategoryId] = useState("");
+    // const [gpa, setGpa] = useState(0);
+    // const [testscore, setTestscore] = useState("");
+    // const [resume, setResume] = useState("");
+    // const [extracurriculars, setExtracurriculars] = useState("");
+    const [checked, setChecked] = useState(false);    
+    const [outcomevalue] = useState('');
+    const [categoryvalue] = useState('');
+    const navigate = useNavigate();
+    const [postData, setPostdata] = useState({
+        title: "",
+        outcome: "",
+        content: "",
+        author: "",
+        category_id: "",
+        gpa: 0,
+        testscore: "",
+        resume: "",
+        extracurriculars: ""
+    });
+
+    const handlePost = async () => {
+        await postapi.createPost(postData)
+        navigate("/feed", { state: { postData } });
+    }
+    
     return (
-        <div>
-        <Header/>
-        
-        <h2>Create a post</h2>
-        <Container size="lg">
+        <>
+            <Container size="lg">
+            {JSON.stringify(postData)}
 
-        <Menu shadow="md" width={650}>
-            <Menu.Target>
-                <Button>Categories</Button>
-            </Menu.Target>
-            <Menu.Dropdown>
-                <Menu.Item>Medical School</Menu.Item>
-                <Menu.Item>Software Engineering</Menu.Item>
-                <Menu.Item>Consulting</Menu.Item>
-                <Menu.Item>Graduate Programs</Menu.Item>
-                <Menu.Item>Other Engineering Professions</Menu.Item>
-            </Menu.Dropdown>
-        </Menu>
-        </Container>
-        <Container>
-        <TextInput
-            placeholder="Your post title"
-            label="Post title"
-            withAsterisk
-        />
-        <TextInput
-            placeholder="Your GPA"
-            label="GPA"
-            withAsterisk
-        />
-        <TextInput
-            placeholder="Your test score"
-            label="Test score"
-            withAsterisk
-        />
-        <FileInput
-            placeholder="Pick file"
-            label="Your resume"
-        />
-        <FileInput
-            placeholder="Pick file"
-            label="Your cover letter"
-        />
-        <Textarea
-            placeholder="Add additional comments"
-            label="Additional comments"
-        />
-        </Container>
-        <Divider my="sm" />
-        <Button.Group>
-            <Button>Save draft</Button>
-            <Button>Post</Button>
-        </Button.Group>
+                <h2>Create a post</h2>
+                </Container>
+                <Container>
+                <Select
+                    label="Categories"
+                    outcomevalue={categoryvalue}
+                    placeholder="Pick one"
+                    data={[
+                        { value: 'Medical School', label: 'Medical School'},
+                        { value: 'Software Engineering', label: 'Software Engineering'},
+                        { value: 'Consulting', label: "Consulting"},
+                        { value: 'Graduate Programs', label: 'Graduate Programs'},
+                        { value: 'Other Engineering Professions', label: 'Other Engineering Professions'}
+                    ]}
+                    onChange={(categoryvalue) => setPostdata({ ...postData, category_id: categoryvalue})}
+                    withAsterisk
+                />
+                <TextInput
+                    placeholder="Your post title"
+                    label="Post title"
+                    onChange={(e) => setPostdata({ ...postData, title: e.target.value})}
+                    withAsterisk
+                />
+                <Select
+                    label="Outcome"
+                    outcomevalue={outcomevalue}
+                    placeholder="Pick one"
+                    data={[
+                        { value: 'Accepted', label: 'Accepted'},
+                        { value: 'Waitlisted', label: 'Waitlisted'},
+                        { value: 'Ghosted', label: "Ghosted"},
+                        { value: 'Rejected', label: 'Rejected'}
+                    ]}
+                    onChange={(outcomevalue) => setPostdata({ ...postData, outcome: outcomevalue})}
+                    withAsterisk
+                />
+                <Textarea
+                    placeholder="Add comments"
+                    label="Comments"
+                    autosize
+                    onChange={(e) => setPostdata({ ...postData, content: e.target.value})}
+                    withAsterisk
+                />
+                <TextInput
+                    placeholder="Your GPA"
+                    label="GPA"
+                    onChange={(e) => setPostdata({ ...postData, gpa: e.target.value})}
+                    withAsterisk
+                />
+                <TextInput
+                    placeholder="Your test score"
+                    label="Test score"
+                    onChange={(e) => setPostdata({ ...postData, testscore: e.target.value})}
+                    withAsterisk
+                />
+                <FileInput
+                    placeholder="Pick file"
+                    label="Your resume"
+                    onChange={(e) => setPostdata({ ...postData, resume: e.target.value})}
+                />
+                <Textarea
+                    placeholder="Your extracurriculars"
+                    label="Extracurriculars"
+                    autosize
+                    onChange={(e) => setPostdata({ ...postData, extracurriculars: e.target.value})}
+                    withAsterisk
+                />
+                <Checkbox label="Anonymous?" checked={checked} onChange={(event) => setChecked(event.currentTarget.checked)}/>
 
-        </div>
+            </Container>
+            <Divider my="sm" />
+            <div className="flex-end">
+                <Button.Group>
+                    <Button>Save draft</Button>
+                    <Button onClick={handlePost}>Post</Button>
+                </Button.Group>
+            </div>
+        </>
 
     )
 }
