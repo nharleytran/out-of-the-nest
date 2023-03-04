fa6ddd5968bec5e3b8fcd7557de831a5191953c1
Anh <anhddo93@gmail.com>
2023-03-04 12:16:31 -0500

    db connect take uri as an argument

--- server/src/data/db.js
+++ server/src/data/db.js
@@ -1,18 +1,12 @@
 import * as dotenv from 'dotenv' 
 import mongoose from "mongoose";
 
-dotenv.config()
-
-// const URI = "mongodb+srv://outofthenest:IBf1pTvKcdAvOtgm@outofthenest.3ji0djw.mongodb.net/dev?retryWrites=true&w=majority";
-const URI = process.env.DB_URI;
-
-console.log('line8', URI);
 const option = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 };
 
-export function connect() {
+export function connect(URI) {
   mongoose.connect(URI, option);
 
   mongoose.connection.on("error", (err) => {
