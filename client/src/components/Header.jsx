b22f3c2e15a2f21cd9b23071f5babb0d22642bb7
Stephen <stephenzhang003@gmail.com>
2023-03-09 15:49:58 -0500

    Added search bar to header and only show search bar to the feed page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,16 +1,24 @@
 import { Button, Text } from '@mantine/core';
 import '../App.css';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
+import Search from "../components/Search";
 
-
-function Header() {
+function Header(props) {
   const navigate = useNavigate();
+  const location = useLocation();
+  const { query, setQuery } = props;
+
+  const isFeedPage = location.pathname ==='/feed'
+  const searchComponent = isFeedPage ? <Search query={query} setQuery={setQuery}/> : null;
+
   return (
     <div className="header">
     <Text size="lg" weight={700} sx={{ fontSize: '29px' }} className="header-text">
       Out of the Nest
     </Text>
     <div style={{ display: "flex", gap: "10px" }}>
+        {searchComponent}
+
         <Button onClick={() => navigate("/create")}>Create post</Button>
         <Button className="login-button">
         Login
