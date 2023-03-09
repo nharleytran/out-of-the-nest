833a40f868bc4a30e7c4afa1bc7bebfddabb94c6
Stephen <stephenzhang003@gmail.com>
2023-03-09 14:42:19 -0500

    Added search bar to categories

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -2,6 +2,7 @@ import { Input } from '@mantine/core';
 import SearchIcon from '@mui/icons-material/Search';
 function Search() {
     return (
+        
         <Input
         icon={<SearchIcon/>}
         placeholder="Look for posts"
