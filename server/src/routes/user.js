a9077dac67922fb2999c16f9c278daecbe93dd7b
Anh <ado8@jhu.com>
2023-03-09 23:49:14 -0500

    format user.js

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -4,23 +4,19 @@ import UserDAO from "../data/UserDAO.js";
 const router = express.Router();
 export const userDao = new UserDAO();
 
-router.post('/user/create', async (req, res) => {
-  const {email, name, password_hash } = req.body;
-    console.log('router post data', email, name, password_hash);
-    console.log('router post data', req.body);
+router.post("/user/create", async (req, res) => {
+  const { email, name, password_hash } = req.body;
   try {
-    const user = await userDao.createUser({ email, name, password_hash});
-      console.log('router post user', user);
+    const user = await userDao.createUser({ email, name, password_hash });
     res.json({
       status: 201,
-     message: `Successfully created user "${user.email}"`,
-      data: user
+      message: `Successfully created user "${user.email}"`,
+      data: user,
     });
   } catch (error) {
-      //console.log('router post error', error);
+    //console.log('router post error', error);
     res.status(500).json({ message: error.message });
   }
 });
-
 
 export default router;
