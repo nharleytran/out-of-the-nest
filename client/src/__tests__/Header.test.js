f020b1771fded6ab0e4a430fb72bb9b9be9e44b4
Stephen <stephenzhang003@gmail.com>
2023-03-10 16:39:20 -0500

    Updated header tests

--- client/src/__tests__/Header.test.js
+++ client/src/__tests__/Header.test.js
@@ -1,36 +1,3 @@
-// import { render, screen, fireEvent } from '@testing-library/react';
-// import Header from '../components/Header';
-
-// jest.mock('react-router-dom', () => ({
-//   useNavigate: jest.fn(),
-// }));
-
-// describe('Header', () => {
-
-//     test('App title text is rendered', () => {
-//         render(<Header />);
-//         expect(screen.getByText('Out of the Nest')).toBeInTheDocument();
-//       });
-
-//     test('Login, sign up, and create post buttons are rendered', () => {
-//         render(<Header />);
-//         expect(screen.getByText('Login')).toBeInTheDocument();
-//         expect(screen.getByText('Sign Up')).toBeInTheDocument();
-//         expect(screen.getByText('Create post')).toBeInTheDocument();
-//     });
-
-//     test('Clicking on create post button navigates to the create post page', () => {
-//         const navigateMock = jest.fn();
-//         jest.spyOn(require('react-router-dom'), 'useNavigate').mockReturnValue(navigateMock);
-
-//         render(<Header />);
-
-//         const createPostButton = screen.getByText('Create post');
-//         fireEvent.click(createPostButton);
-
-//         expect(navigateMock).toHaveBeenCalledWith('/create');
-//     });
-// });
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 import { useNavigate, useLocation } from 'react-router-dom';
@@ -39,18 +6,13 @@ import Header from '../components/Header';
 jest.mock('react-router-dom', () => ({
   useNavigate: jest.fn(),
   useLocation: jest.fn(),
+  Link: jest.fn(({ to, children }) => <a href={to}>{children}</a>),
 }));
 
 describe('Header component', () => {
   beforeEach(() => {
     useNavigate.mockClear();
     useLocation.mockClear();
-  });
-
-  test('App title text is rendered', () => {
-    useLocation.mockReturnValueOnce({ pathname: '/' });
-    const { getByText } = render(<Header />);
-    expect(getByText('Out of the Nest')).toBeInTheDocument();
   });
 
   test('Renders search component when on feed page', () => {
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,32 +1,34 @@
-import { Button, Text } from '@mantine/core';
+import { Button, Text, Title } from '@mantine/core';
 import '../App.css';
-import { useNavigate, useLocation } from 'react-router-dom';
+import { useNavigate, useLocation, Link } from 'react-router-dom';
 import Search from "../components/Search";
-
+import Filter from "../components/FilterContent/Filter";
 function Header(props) {
   const navigate = useNavigate();
   const location = useLocation();
-  const { query, setQuery } = props;
-
+  const { query, setQuery, onFilterClick } = props;
   const isFeedPage = location.pathname ==='/feed'
   const searchComponent = isFeedPage ? <Search query={query} setQuery={setQuery}/> : null;
+  const filterComponent = isFeedPage ? <Filter onFilterClick={onFilterClick}/> : null;
 
   return (
     <div className="header">
-    <Text size="lg" weight={700} sx={{ fontSize: '29px' }} className="header-text">
-      Out of the Nest
-    </Text>
-    <div style={{ display: "flex", gap: "10px" }}>
-        {searchComponent}
+      <Link to={`/`} style={{ textDecoration: 'none' }}>
+        <Title order={3}> Out of the <Text span c="blue" inherit>Nest</Text></Title>
+      </Link>
+      <div style={{ display: "flex", gap: "10px" }}>
+          {searchComponent}
 
-        <Button onClick={() => navigate("/create")}>Create post</Button>
-        <Button className="login-button">
-        Login
-        </Button>
-        <Button className="login-button">
-        Sign Up
-        </Button>
-    </div>
+          {filterComponent}
+
+          <Button onClick={() => navigate("/create")}>Create post</Button>
+          <Button className="login-button">
+          Login
+          </Button>
+          <Button className="login-button">
+          Sign Up
+          </Button>
+      </div>
     </div>
   );
 }
