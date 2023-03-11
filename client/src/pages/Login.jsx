96e75fa512c455f0613f897d3e230a007b7b9767
Anh <ado8@jhu.edu>
2023-03-11 00:20:55 -0500

    update notification for invalid email and password

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,85 +1,128 @@
-import { useState, useEffect } from "react";
+import {
+    useState,
+    useEffect
+} from "react";
 import * as postapi from "../api/index";
-import { hashPassword } from "../api/util/password.js";
-import { useNavigate } from "react-router-dom";
-import { Anchor, Flex } from "@mantine/core";
+import {
+    hashPassword
+} from "../api/util/password.js";
+import {
+    useNavigate
+} from "react-router-dom";
+import {
+    Anchor,
+    Flex
+} from "@mantine/core";
 
 import {
-  Box,
-  Input,
-  Checkbox,
-  Group,
-  TextInput,
-  Button,
-  PasswordInput,
+    Box,
+    Input,
+    Checkbox,
+    Group,
+    TextInput,
+    Button,
+    PasswordInput,
 } from "@mantine/core";
-import { sha256, sha224 } from "js-sha256";
-import { useForm } from "@mantine/form";
-import { MantineProvider } from "@mantine/core";
-import { notifications } from "@mantine/notifications";
+import {
+    sha256,
+    sha224
+} from "js-sha256";
+import {
+    useForm
+} from "@mantine/form";
+import {
+    MantineProvider
+} from "@mantine/core";
+import {
+    notifications
+} from "@mantine/notifications";
 
 function Login() {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const form = useForm({
-    initialValues: {
-      email: "",
-      password: "",
-    },
+    const form = useForm({
+        initialValues: {
+            email: "",
+            password: "",
+        },
 
-    validate: {
-      email: (value) => (/^\S+@\S+$/.test(value) ? null : "Invalid email"),
-    },
-  });
-  const handleLogin = async (userFormData) => {
-    try {
-      const response = await postapi.login(userFormData);
-      console.log(response);
-    if (response.status === 200) {
-        const token = response.data.token;
-        localStorage.setItem("token", token);
-        postapi.axiosInstance.defaults.headers['Authorization'] = `Bearer ${localStorage.getItem("token")}`;
-        postapi.testAuthorize();
-        notifications.show({
-          title: "Login successfully",
-          message: "Redirecting to home page",
-            onClose: () => navigate("/"),
-            loading:true,
-            position: "top-right",
-        });
-      }
-    } catch (err) {
-      console.log(err);
-    }
-  };
-  return (
-    <>
-      <Box maw={300} mx="auto">
-        <form onSubmit={form.onSubmit(handleLogin)}>
-          <TextInput
-            withAsterisk
-            label="Email"
-            placeholder="your@email.com"
-            value="your@email.com"
-            {...form.getInputProps("email")}
-          />{" "}
-          <PasswordInput
-            placeholder="password"
-            label="Password"
-            value="sfdf"
-            {...form.getInputProps("password")}
-            withAsterisk
-          />
-          <Flex justify="flex-end">
-            <Anchor href="/user/create">Create new account</Anchor>
-          </Flex>
-          <Group position="right" mt="md">
-            <Button type="submit"> Sign in </Button> <Button> Cancel </Button>{" "}
-          </Group>{" "}
-        </form>{" "}
-      </Box>
-    </>
-  );
+        validate: {
+            email: (value) => (/^\S+@\S+$/.test(value) ? null : "Invalid email"),
+        },
+    });
+    const handleLogin = async (userFormData) => {
+        try {
+            const response = await postapi.login(userFormData);
+            console.log(response);
+            if (response.status === 200) {
+                const token = response.data.token;
+                localStorage.setItem("token", token);
+                postapi.axiosInstance.defaults.headers['Authorization'] = `Bearer ${localStorage.getItem("token")}`;
+                notifications.show({
+                    title: "Login successfully",
+                    message: "Redirecting to home page",
+                    autoClose: 3000,
+                    onClose: () => navigate("/"),
+                    loading: true,
+                    position: "top-right",
+                });
+            }
+        } catch (err) {
+            notifications.show({
+                id: 'hello-there',
+                withCloseButton: true,
+                autoClose: 5000,
+                title: "Login failed",
+                message: 'Invalid email or password',
+                color: 'red',
+                className: 'my-notification-class',
+                loading: false,
+            });
+
+            console.log(err);
+        }
+    };
+    return ( <
+        >
+        <
+        Box maw = {
+            300
+        }
+        mx = "auto" >
+        <
+        form onSubmit = {
+            form.onSubmit(handleLogin)
+        } >
+        <
+        TextInput withAsterisk label = "Email"
+        placeholder = "your@email.com"
+        value = "your@email.com" {
+            ...form.getInputProps("email")
+        }
+        />{" "} <
+        PasswordInput placeholder = "password"
+        label = "Password"
+        value = "sfdf" {
+            ...form.getInputProps("password")
+        }
+        withAsterisk /
+        >
+        <
+        Flex justify = "flex-end" >
+        <
+        Anchor href = "/user/create" > Create new account < /Anchor> <
+        /Flex> <
+        Group position = "right"
+        mt = "md" >
+        <
+        Button type = "submit" > Sign in < /Button> <Button> Cancel </Button > {
+            " "
+        } <
+        /Group>{" "} <
+        /form>{" "} <
+        /Box> <
+        />
+    );
 }
 
 export default Login;
