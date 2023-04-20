034bb4f5393ab1c07dd0d02a93e0743a24dcfa4a
Anh <ado8@jhu.edu>
2023-04-20 15:38:52 -0400

    change the layout

--- src/pages/Results.jsx
+++ src/pages/Results.jsx
@@ -62,52 +62,6 @@ function Results() {
                 out of 4.0
               </Text>
             </div>
-            <div className="flipInX animated results-circle results-circle1">
-              <Text align="center" mt={25}>
-                Your test score is
-              </Text>
-              {/^[^0-9]*$/.test(testscore) ? (
-                <>
-                  <Avatar
-                    size="xl"
-                    className="results-avatar"
-                    style={{ color: '#000' }}>
-                    {testscore}
-                  </Avatar>
-                  <Text align="center" style={{ marginTop: 10 }}>
-                    out of {testscore}
-                  </Text>
-                </>
-              ) : (
-                <>
-                  <Avatar
-                    size="xl"
-                    className="results-avatar"
-                    style={{ color: '#000' }}>
-                    {testscore}
-                  </Avatar>
-                  <Text align="center" style={{ marginTop: 10 }}>
-                    out of 1600
-                  </Text>
-                </>
-              )}
-            </div>
-            <div className="extracurriculars extracurriculars1">
-              <Title align="center" order={3} pt={10}>
-                Your extracurriculars
-              </Title>
-              <ScrollArea h={280} mt={12}>
-                <ol>
-                  {extracurriculars.map((extracurricular, index) =>
-                    extracurricular.content.length >= 1 ? (
-                      <li key={extracurricular.key}>
-                        <Badge>{extracurricular.content}</Badge>
-                      </li>
-                    ) : null
-                  )}
-                </ol>
-              </ScrollArea>
-            </div>
           </Grid.Col>
           <Grid.Col span={4}>
             <div className="flipInX animated results-circle results-circle2">
@@ -128,110 +82,12 @@ function Results() {
                 </Text>
               </Card.Section>
             </div>
-            <div className="flipInX animated results-circle results-circle2">
-              <Card.Section>
-                <Text align="center" mt={25}>
-                  Average test score is
-                </Text>
-              </Card.Section>
-              {/^[^0-9]*$/.test(hardCodedTestScore) ? (
-                <>
-                  <Avatar
-                    size="xl"
-                    className="results-avatar"
-                    style={{ color: '#000' }}>
-                    {hardCodedTestScore}
-                  </Avatar>
-                  <Text align="center" style={{ marginTop: 10 }}>
-                    out of {hardCodedTestScore}
-                  </Text>
-                </>
-              ) : (
-                <>
-                  <Avatar
-                    size="xl"
-                    className="results-avatar"
-                    style={{ color: '#000' }}>
-                    {hardCodedTestScore}
-                  </Avatar>
-                  <Text align="center" style={{ marginTop: 10 }}>
-                    out of 1600
-                  </Text>
-                </>
-              )}
-            </div>
-            <div className="extracurriculars extracurriculars2">
-              <Title align="center" order={3} pt={10}>
-                Other people's extracurriculars
-              </Title>
-              <ScrollArea h={280} mt={12}>
-                <ol>
-                  {hardCodedExtracurricular.map((extracurricular, index) =>
-                    extracurricular.content.length >= 1 ? (
-                      <li key={extracurricular.key}>
-                        <Badge>{extracurricular.content}</Badge>
-                      </li>
-                    ) : null
-                  )}
-                </ol>
-              </ScrollArea>
-            </div>
           </Grid.Col>
           <Grid.Col span={4}>
             <Card>
               <ScrollArea h={190}>
-                <Title order={3}>Improving your GPA</Title>
+                <Title order={3}>Our suggestion</Title>
                 <Text>
-                  The number one thing that is essential to improving your GPA
-                  is attending your classes regularly! You won't miss any
-                  detailed explanations from the professor and it allows you to
-                  participate in class. Being in class will help you in many
-                  ways. For example, the classes will supplement your learning.
-                  You will be able to cut down on your study time later with how
-                  much you have learned and retained from attending class.{' '}
-                </Text>
-                <Text>
-                  Join a study group! You can get your friend or classmates
-                  together to study together in a class. Its important to have a
-                  support system where you can share notes, discuss different
-                  class topics, and ask questions to each other.
-                </Text>
-              </ScrollArea>
-            </Card>
-            <Card mt={20}>
-              <ScrollArea h={190}>
-                <Title order={3}>Improving your test score</Title>
-                <Text>
-                  Cultivate good study habits. We often cannot study the day
-                  before an exam to do well. Developing good study habits will
-                  help you in performing better on exams. This process includes
-                  steps such as reviewing your notes frequently and doing your
-                  homework assignments.{' '}
-                </Text>
-                <Text>
-                  Get a good night's sleep. Make sleep a priority as it will
-                  help you think more clearly during the test. In addition,
-                  having a good sleep habit has been shown to consolidate memory
-                  and improve academic performance.
-                </Text>
-              </ScrollArea>
-            </Card>
-            <Card mt={20}>
-              <ScrollArea h={190}>
-                <Title order={3} mt={10}>
-                  Improving your extracurriculars
-                </Title>
-                <Text>
-                  Look to volunteer in your local community! Doing volunteer
-                  work demonstrates leadership and passion. It can be a good way
-                  to show reviewers of your application to jobs or schools in
-                  what type of person you are.
-                </Text>
-                <Text>
-                  If you are applying for different jobs, make sure you apply to
-                  a lot. Often, it takes days or weeks for a job application to
-                  give an update back to you. Make sure you keep your options
-                  open.{' '}
                 </Text>
               </ScrollArea>
             </Card>
