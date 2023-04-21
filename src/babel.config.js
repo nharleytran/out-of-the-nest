a51c430bb58bfb87ddf55c537a7e81f6c20d506d
Anh <ado8@jhu.edu>
2023-04-21 19:07:02 -0400

    fix jest import error

--- src/babel.config.js
+++ src/babel.config.js
@@ -7,4 +7,5 @@ module.exports = {
   transformIgnorePatterns: [
   // Change MODULE_NAME_HERE to your module that isn't being compiled
   "/node_modules/(?!axios).+\\.js$"
+  ]
 }
--- src/jest.config.js
+++ src/jest.config.js
@@ -1,9 +1,13 @@
 module.exports = {
-  presets: ["@babel/preset-env", "@babel/preset-react"]
+  presets: ["@babel/preset-env", "@babel/preset-react"],
   transform: {
     "^.+\\.(js|jsx)$": "babel-jest",
   },
-  transformIgnorePatterns: [
-  // Change MODULE_NAME_HERE to your module that isn't being compiled
-    "node_modules/(?!(axios)/)"
+  transformIgnorePatterns: ['/node_modules/(?!(axios)/)'],
+  moduleNameMapper: {
+    '^axios$': require.resolve('axios'),
+  },
 };
+
+
+module.exports = config;
